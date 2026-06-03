ca2304938a7d2c1f04f302aec8eb6187519b8ea7
NguyenBat-Phuong <nguyenbatphuong@gmail.com>
2026-06-03 20:54:12 +0700

    Initial commit

---
+++ test.cs
@@ -0,0 +1,11 @@
+namespace ConsoleApp1;
+
+public class Test
+{
+    public Test()
+    {
+        string number = "58";
+        int num= Int32.Parse(number);
+        Console.WriteLine(num);
+    }
+}
